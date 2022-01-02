309bca8ac6129e7182e14b1eb668de85624d9d2f
Konrad Mösch <konrad@moesch.org>
2022-01-02 02:56:01 +0100

    Add error handling when config invalid

--- software/nixie-clock/src/StorageController.cpp
+++ software/nixie-clock/src/StorageController.cpp
@@ -20,9 +20,20 @@ Timezone_t StorageController::getTimezoneConfig() {
 void StorageController::storeTimezoneConfig(Timezone_t timezoneConfig) {
     prefs.begin(PREFS_NAMESPACE);
     prefs.putBytes(PREFS_KEY, &timezoneConfig, sizeof(Timezone_t));
+    prefs.putBool("VALID", true);
     prefs.end();
+}
+
+bool validTZConfig() {
+    prefs.begin(PREFS_NAMESPACE, true);
+    bool value = prefs.getBool("VALID");
+    prefs.end();
+    return value;
 }
 
 void StorageController::initialize() {
     Log.noticeln("Initializing Storage");
+    if (!validTZConfig()) {
+        storeTimezoneConfig({"Europe/London","europe.pool.ntp.org",0});
+    }
 }
